ec27fd846bfd2e43ea46991ccf0a01b7b0ddbcfb
makstheimba <makstheimba@yandex.ru>
2016-10-14 19:26:05 +0300

    Styling and code refactoring

--- assets/tictactoe.js
+++ assets/tictactoe.js
@@ -1,63 +1,79 @@
-$(document).ready(function () {
-    game = tictactoe("X");
-    game.newGame();
+$(window).resize(function () {
+    var fontSize = $("#playField").width() * 0.3 + "px";
+    $(".content").css("font-size", fontSize);
 });
-var tictactoe = function (firstTurn) {
-    var nextTurn = {"X": "O", "O": "X"},
-        score = {"redPlayer": 0, "bluePlayer": 0},
-        valueToPlayer = {},
+
+var tictactoe = function (players) {
+    var nextMarker = {"X": "O", "O": "X"},
         field = Array(9),
-        currentTurn = firstTurn,
+        currentMarker = "X",
+        currentPlayer = players.markerToPlayer[currentMarker],
+        celebrationLength = 1000,
         self = {};
-    // Setting up chosen values for the players ES5 way
-    valueToPlayer[firstTurn] = "redPlayer";
-    valueToPlayer[nextTurn[firstTurn]] = "bluePlayer";
+    $("#"+currentPlayer).addClass(currentPlayer+"Active");
+    
+    function isMarkerInPlace(place) {
+        return field[place] === currentMarker;
+    }
     function testWinner() {
-        var winConditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-        winConditions.forEach(function(state) { // Test if all places on a field for a certain win condition occupied with the same element            
-            if (state.every(isTurnInPlace)) {
-                var winner = valueToPlayer[currentTurn];
+        var winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+                             [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+        winConditions.forEach(function (state) { // Test if all places on a field for a certain win condition occupied with the same element            
+            if (state.every(isMarkerInPlace)) {
+                var winner = currentPlayer;
                 alert("Winner is " + winner);
-                score[winner] += 1;
+                players.score[winner] += 1;
                 self.newGame();
             }
         });
-        if (field.every(function(elem) {return elem !==""})){ //If every cell is occupied call a tie
+        if (field.every(function (elem) {return elem !== ""; })) { //If every cell is occupied call a tie
             alert("Tie");
             self.newGame();
         }
-    }    
-    function isTurnInPlace(place) {
-        return field[place] === currentTurn;
     }
-    function setValue(place) {
-        field[place] = currentTurn;
+    function setMarker(place) {
+        $("#"+currentPlayer).removeClass(currentPlayer+"Active");
+        field[place] = currentMarker;
         testWinner();
-        currentTurn = nextTurn[currentTurn];
+        currentMarker = nextMarker[currentMarker];
+        currentPlayer = players.markerToPlayer[currentMarker];
+        $("#"+currentPlayer).addClass(currentPlayer+"Active");
     }
     self.newGame = function () {
-        var i;
-        for (i = 0; i < 9; i++) {
-            $("#cell"+i).off("click"); //Clear clicks;
-            $("#cell"+i).addClass("activeCell");
-            $("#cell"+i).find(".content").html("");
-        }
-        field.fill(""); // Empty playing field
-        $("#redPlayer").html(score.redPlayer);
-        $("#bluePlayer").html(score.bluePlayer);
-        for (i = 0; i < 9; i++) {
-            $("#cell"+i).on("click", function () {
-                $(this).find(".content").html(currentTurn);
-                $(this).off("click");
-                $(this).removeClass("activeCell");
-                setValue($(this).attr("id").slice(-1)); // Set value to a chosen place on a field
+        // Update score
+        $("#redPlayer").html(players.score.redPlayer);
+        $("#bluePlayer").html(players.score.bluePlayer);
+        // Setup playing field
+        field.fill("").forEach(function (_, i) {
+            var cellId = "#cell" + i;
+            $(cellId).off("click").addClass("activeCell"); //Clear previous clicks
+            $(cellId).find(".content").html(""); //Empty all cells
+            $(cellId).removeClass("bluePlayer redPlayer"); // Remove color markers
+            
+            // Add brand new clicks
+            $(cellId).on("click", function () {
+                $(this).find(".content").html(currentMarker);
+                $(this).addClass(currentPlayer);
+                $(this).off("click").removeClass("activeCell");
+                setMarker($(this).attr("id").slice(-1)); // Set value to a chosen cell on a field
             });
-        }
-    }
+        });
+    };
     self.resetScore = function () {
-        score.redPlayer = score.bluePlayer = 0;
-        $("#redPlayer").html(score.redPlayer);
-        $("#bluePlayer").html(score.bluePlayer);
-    }    
-    return self;    
-}
+        players.score.redPlayer = players.score.bluePlayer = 0;
+        $("#redPlayer").html(players.score.redPlayer);
+        $("#bluePlayer").html(players.score.bluePlayer);
+    };
+    return self;
+};
+
+$(document).ready(function () {
+    var players = {score: {"redPlayer": 0, "bluePlayer": 0}, markerToPlayer: {}},
+        game;
+    players.markerToPlayer["X"] = "redPlayer";
+    players.markerToPlayer["O"] = "bluePlayer";
+    
+    $(".content").css("font-size", $("#playField").width() * 0.3 + "px");
+    game = tictactoe(players);
+    game.newGame();
+});
